24e52f03ce49e11628ab95f6340c10df4d973972
saidmsaif <saidmsaiff@gmail.com>
2021-08-09 06:04:04 +0600

    shuffled array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const inputNames = document.getElementById('names')
-const form = document.getElementById('form')
-const nameList = document.getElementById('nameList')
+const inputNames        = document.getElementById('names')
+const form              = document.getElementById('form')
+const nameList          = document.getElementById('nameList')
+const spinBtn           = document.getElementById('spin-a-try')
 
 const participants = []
 
@@ -14,7 +15,6 @@ form.addEventListener('submit', (e) => {
             participants.push(name)
             let item = createListItem(name)
             nameList.appendChild(item)
-            console.log(participants);
             inputNames.value = ''
         })
     }
@@ -27,3 +27,25 @@ function createListItem(name) {
 
     return listItem
 }
+
+spinBtn.addEventListener('click', () => {
+    if(participants.length === 0) {
+        alert('No participants!')
+    }
+})
+
+function shuffle(args) {
+    let shuffledArr = [...args]
+
+    for(let i = shuffledArr.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1))
+
+        let temp = shuffledArr[rand]
+        shuffledArr[rand] = shuffledArr[i]
+        shuffledArr[i] = temp
+    }
+
+    return shuffledArr
+}
+
+console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
